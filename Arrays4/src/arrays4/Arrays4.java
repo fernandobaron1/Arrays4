0988db9db5068aa16d4852e07d4d19b45ead86e5
Fernando Baron <fernandobaron1@hotmail.com>
2019-08-10 23:27:28 -0300

    Modelo 1

---
+++ Arrays4/src/arrays4/Arrays4.java
@@ -0,0 +1,23 @@
+
+package arrays4;
+
+
+public class Arrays4 {
+
+   
+    public static void main(String[] args) {
+        
+        String [] listaDeProdutos =new String[5];
+        
+        listaDeProdutos [0] = "Mouse";
+        listaDeProdutos [1] = "Teclado";
+        listaDeProdutos [2] = "Monitor";
+        listaDeProdutos [3] = "Processador";
+        listaDeProdutos [4] = "Pen drive";
+        
+        for(int i = 0; i < listaDeProdutos.length; i++) {
+        System.out.println (listaDeProdutos[i]);
+        
+    }
+    }  
+}
